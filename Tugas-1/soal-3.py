e3fed5ef90902d15eb248827c0a55e96c2691d5c
Tiiaraa <[dewi.mutiara606@gmail.com]>
2021-01-10 21:28:55 -0800

    [Assign1]

--- Tugas-1/soal-3.py
+++ Tugas-1/soal-3.py
@@ -3,9 +3,9 @@ praktek=int(input("Masukkan nilai ujian praktek: "))
 
 if(teori>=70 and praktek>=70):
     print("Selamat, anda lulus!")
-elif(teori>=70 and praktek<=70):
+elif(teori>=70 and praktek<70):
     print("Anda harus mengulang ujian praktek.")
-elif(teori<=70 and praktek>=70):
+elif(teori<70 and praktek>=70):
     print("Anda harus mengulang ujian teori")
 else:
     print("Anda harus mengulang ujian teori dan praktek")
