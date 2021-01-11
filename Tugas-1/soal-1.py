07f095f799ed19fa271a79ad2b10622aca6f4151
Tiiaraa <[dewi.mutiara606@gmail.com]>
2021-01-10 21:22:49 -0800

    [Assignment 1]

---
+++ Tugas-1/soal-1.py
@@ -0,0 +1,4 @@
+nama = "Dewi Mutiara Nurani"
+umur =  17
+tinggi=160.5
+print("Nama saya {}, umur saya {} tahun dan tinggi saya {} cm".format(nama,umur,tinggi))
---
+++ Tugas-1/soal-2.py
@@ -0,0 +1,6 @@
+phi = 22/7
+r = int(input("r= "))
+Luas=phi*(r**2)
+
+
+print("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2 " .format(r,Luas))
